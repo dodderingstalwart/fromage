b92d3798e7638df502b1b8f5eb095a4a0c40957f
dodderingstalwart <bardadreamer@gmail.com>
2025-10-17 23:35:20 -0700

    Added gitignore

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"os"
 
-	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
 )
@@ -15,20 +14,19 @@ func main() {
 	fmt.Println("Fromage")
 	ctx := context.Background()
 
-	b, err := google.CredentialsFromJSON(ctx, []byte(`{
-		"type": "service_account",
-	}`), sheets.SpreadsheetsReadonlyScope)
-	if err != nil {
-		log.Fatalf("Unable to parse credentials from JSON: %v", err)
-	}
-
-	srv, err := sheets.NewService(ctx, option.WithCredentials(b))
+	// Initialize the Sheets API client
+	srv, err := sheets.NewService(ctx, option.WithCredentialsFile("credentials.json"))
 	if err != nil {
 		log.Fatalf("Unable to retrieve Sheets client: %v", err)
 	}
 
+	// The ID of the spreadsheet to retrieve data from.
 	spreadsheetId := os.Getenv("GSHEETID")
+
+	// The range of cells to read from the spreadsheet.
 	readRange := "Sheet1!A1:D10"
+
+	// Retrieve data from the specified range
 	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve data from sheet: %v", err)
